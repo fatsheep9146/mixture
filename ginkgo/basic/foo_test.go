99893b79d40d2e0ca7d8a038205115d4671fc820
fatsheep9146 <zhaoziqi@qiniu.com>
2018-12-22 10:29:28 +0800

    modify test

--- ginkgo/basic/foo_test.go
+++ ginkgo/basic/foo_test.go
@@ -1,25 +1,38 @@
 package foo_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	//	. "github.com/onsi/gomega"
 
 	. "github.com/fatsheep9146/mixture/ginkgo/basic"
 )
 
 var _ = Describe("Foo", func() {
+	var f Foo
+
+	BeforeEach(func() {
+		f = Foo{
+			Bar: "bar",
+		}
+	})
 
 	Context("when it is at the weekend", func() {
+		f.Bar = "dog"
+
 		It("should be a fool", func() {
+			f.Bar = "cat"
+			fmt.Printf(Display(f))
 		})
 
 		It("should not work", func() {
-
+			fmt.Printf(Display(f))
 		})
 	})
 
 	Context("when it is in the weekday", func() {
 		It("should not be a fool", func() {
+			fmt.Printf(Display(f))
 		})
 	})
 
