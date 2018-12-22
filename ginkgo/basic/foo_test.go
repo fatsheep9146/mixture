a33ec0d9fb54e3877737db4fffec793fe4ff8ed0
fatsheep9146 <zhaoziqi@qiniu.com>
2018-12-22 10:49:40 +0800

    update basic

--- ginkgo/basic/foo_test.go
+++ ginkgo/basic/foo_test.go
@@ -62,3 +62,44 @@ var _ = Describe("Foo", func() {
 	})
 
 })
+
+/*
+STEP: Context level [By] 0
+STEP: Context level [By] 1
+Running Suite: Basic Suite
+==========================
+Random Seed: 1545446770
+Will run 4 of 4 specs
+
+Foo when it is at the weekend
+  should be a fool
+  /root/go/src/github.com/fatsheep9146/mixture/ginkgo/basic/foo_test.go:33
+STEP: It level [By] 0-0
+Foo f: fbar2
+Foo g: gbar1
+•
+------------------------------
+Foo when it is at the weekend
+  should not work
+  /root/go/src/github.com/fatsheep9146/mixture/ginkgo/basic/foo_test.go:40
+STEP: It level [By] 0-1
+Foo f: fbar1
+Foo g: gbar1
+•
+------------------------------
+Foo when it is in the weekday
+  should not be a fool
+  /root/go/src/github.com/fatsheep9146/mixture/ginkgo/basic/foo_test.go:49
+STEP: It level [By] 1-0
+Foo f: fbar0
+Foo g: gbar1
+•
+------------------------------
+Foo test nested describe when test nested describe
+  should display like what
+  /root/go/src/github.com/fatsheep9146/mixture/ginkgo/basic/foo_test.go:58
+•
+Ran 4 of 4 Specs in 0.000 seconds
+SUCCESS! -- 4 Passed | 0 Failed | 0 Pending | 0 Skipped
+PASS
+*/
